82f91d43ad498e71022e4d5c783f663e95515a80
Chris Roberts <chrisroberts0824@gmail.com>
2025-06-26 09:04:18 +0100

    Implemented ErrorPage.tsx

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,23 @@
+import {isRouteErrorResponse, useRouteError} from 'react-router-dom';
+import Header from '@/components/Header';
+import {GAP, PADDING} from '@/styles/constants';
+import {Box, Heading} from '@chakra-ui/react';
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  console.log(error);
+
+  return (
+    <Box className='wrapper' gap={GAP} padding={PADDING}>
+      <Header />
+      <Heading as='h1'>Oops</Heading>
+      <p>
+        {isRouteErrorResponse(error)
+          ? 'Sorry, that page could not be found.'
+          : `Sorry, an unexpected error occured. ${error}.`}
+      </p>
+    </Box>
+  );
+};
+
+export default ErrorPage;
